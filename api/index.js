c6e542ff1c27ecf48b38dcdeca40fe98ea5fecfb
Rajat Kishore Acharya <rajat.getmeonfb@gmail.com>
2024-02-10 19:01:44 +0530

    Database connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import mongoose, { mongo } from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express();
+
+mongoose.connect(
+    process.env.MONGO
+    ).then(() => {
+        console.log("Database is connected");
+    }).catch((err) => {
+        console.log(err)
+    })
 
 
 app.listen(3000, () => {
